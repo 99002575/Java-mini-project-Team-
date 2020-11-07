e9e0d47b4e3f32b9b6c5320c6d3a6a284be782ff
99002584 <71544562+99002584@users.noreply.github.com>
2020-11-07 10:02:39 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,1 +1,27 @@
+public void getCheckingWithdrawInput(){
+    System.out.println("Current Account Balance: " + moneyFormat.format(checkingBalance));
+    System.out.print("Amount you want to withdraw from Current Account: ");
+    double amount =input.nextDouble();
 
+ 
+
+    if((checkingBalance-amount)>=0){
+      calcCheckingWithdraw(amount);
+      System.out.println("New Current Account Balance: " + moneyFormat.format(checkingBalance));
+      }///
+    }
+
+ 
+
+    public void getsavingWithdrawInput(){
+      System.out.println("Saving Account Balance: " + moneyFormat.format(savingBalance));
+      System.out.print("Amount you want to withdraw from saving Account: ");
+      double amount =input.nextDouble();
+
+ 
+
+      if((savingBalance-amount)>=0){
+        calcSavingWithdraw(amount);
+        System.out.println("New saving Account Balance: " + moneyFormat.format(savingBalance));
+        }///
+      }
