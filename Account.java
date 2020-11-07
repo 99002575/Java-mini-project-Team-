a9ddf5e97917c2abaa8939d697e9b35155e24d43
99002575 <71598125+99002575@users.noreply.github.com>
2020-11-07 11:01:10 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -29,3 +29,36 @@ public void getCheckingWithdrawInput(){
         System.out.println("Balance Cannot be Negative." + "\n");
         }
       }
+
+///// deposition of amount
+public void getCheckingDepositInput(){
+        System.out.println("Checking Account Balance: " + moneyFormat.format(checkingBalance));
+        System.out.print("Amount you want to Deposit from Checking Account: ");
+        double amount =input.nextDouble();
+
+ 
+
+        if((checkingBalance+amount)>=0){
+          calcCheckingDeposit(amount);
+          System.out.println("New Checking Account Balance: " + moneyFormat.format(checkingBalance));
+          }else{
+          System.out.println("Balance Cannot be Negative." + "\n");
+          }
+        }
+
+ 
+
+        public void getSavingDepositInput(){
+          System.out.println("Saving Account Balance: " + moneyFormat.format(savingBalance));
+          System.out.print("Amount you want to Deposit from saving Account: ");
+          double amount =input.nextDouble();
+
+ 
+
+          if((savingBalance+amount)>=0){
+            calcSavingDeposit(amount);
+            System.out.println("New saving Account Balance: " + moneyFormat.format(savingBalance));
+            }else{
+            System.out.println("Balance Cannot be Negative." + "\n");
+            }
+          }
